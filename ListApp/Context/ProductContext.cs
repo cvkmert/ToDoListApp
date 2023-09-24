1cf0eefc2201960ab1959b6dc02b4abd293210a3
PC <PC@DESKTOP-BF36BAV>
2023-09-24 22:21:25 +0300

    Add project files.

---
+++ ListApp/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using ListApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListApp.Context
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
